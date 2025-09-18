769a7422514df179b64e908a8798e46b04efbe99
Gokulraj Tamilvindan <97329414+gokulrajtamilvindan@users.noreply.github.com>
2025-09-18 19:07:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 function firstNonRepeatedChar(str) {
- // Write your code here
+    // Write your code here
+    for (let i = 0; i < str.length; i++) {
+        let char = str[i];
+        if (str.indexof(char) === i && str.indexof(char, i + 1) === -1) {
+            return char;
+        }
+    }
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
